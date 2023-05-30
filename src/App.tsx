c890129544da079aae3b137e13e442e85fadb7f2
carlos rubio <86839953+gitrubio@users.noreply.github.com>
2023-05-29 20:54:32 -0500

    primera fase proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import {
+  AppBar,
+  Box,
+  FilledInput,
+  FormControl,
+  Grid,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Toolbar,
+} from "@mui/material";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import NavComponent from "./components/NavComponent";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ width: "130vh", height: "100%" }}>
+      <Grid container spacing={2}>
+        <NavComponent />
+
+        <Grid item xs={6} md={12} style={{ background : 'red'}}>
+          asd
+        </Grid>
+   
+      </Grid>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/NavComponent.tsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+
+import {
+  Box,
+  FormControl,
+  Grid,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+} from "@mui/material";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+export default function NavComponent() {
+  return (
+    <>
+      <Grid item xs={6} md={8}>
+        <FormControl sx={{ m: 1, width: "100%" }} variant="outlined">
+          <InputLabel htmlFor="search-input">Search</InputLabel>
+          <OutlinedInput
+            id="search-input"
+            type={"text"}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={() => {}}
+                  onMouseDown={() => {}}
+                  edge="end"
+                ></IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
+      </Grid>
+      <Grid
+        item
+        xs={6}
+        md={4}
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <Stack
+          spacing={4}
+          direction="row"
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <Button variant="outlined" size="large" sx={{ borderRadius: 4 }}>
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              height={24}
+              width={24}
+            >
+              <AccountCircleIcon color="primary" scale={2} />
+            </Box>
+          </Button>
+          <Button variant="contained" size="large" sx={{ borderRadius: 4 }}>
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              height={24}
+              width={24}
+            >
+              <AccountCircleIcon color="secondary" scale={2} />
+            </Box>
+          </Button>
+        </Stack>
+      </Grid>
+    </>
+  );
+}
